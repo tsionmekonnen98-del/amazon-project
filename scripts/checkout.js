14b96b688067e021b798203a690b5dc0a9b1d131
tsion <tsionmekonnen98@gmail.com>
2026-03-19 22:03:08 -0700

    Delete products from cart when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart } from "../data/cart.js";
+import { cart, removeFromCart } from "../data/cart.js";
 import { products } from "../data/products.js";
   import { formatCurrency } from "./utils/money.js";
 let cartSummaryHTML = '';
@@ -37,7 +37,7 @@ cart.forEach((cartItem)=>{
             <span class="update-quantity-link link-primary">
             Update
             </span>
-            <span class="delete-quantity-link link-primary">
+            <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
             Delete
             </span>
         </div>
@@ -76,7 +76,7 @@ cart.forEach((cartItem)=>{
         <div class="delivery-option">
             <input type="radio"
             class="delivery-option-input"
-            name="delivery-option-1">
+            name="delivery-option-${matchingProduct.id}">
             <div>
             <div class="delivery-option-date">
                 Monday, June 13
@@ -94,3 +94,11 @@ cart.forEach((cartItem)=>{
 });
 
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
+
+document.querySelectorAll('.js-delete-link').forEach((link) =>{
+ link.addEventListener('click', () =>{
+    const productId = link.dataset.productId;
+    removeFromCart(productId);
+    console.log(cart);
+ });
+});
